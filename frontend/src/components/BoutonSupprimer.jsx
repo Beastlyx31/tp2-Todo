53607f62cf80b6ccb7556b53c7b3b5556da56a6b
Ariane Bohémier <2141577@csfoy.ca>
2025-12-10 13:59:37 -0500

    modifs

--- frontend/src/components/BoutonSupprimer.jsx
+++ frontend/src/components/BoutonSupprimer.jsx
@@ -7,8 +7,7 @@ function BoutonSupprimer(props) {
         <>
             <button onClick={(e) => {
                 deleteTask(props.id);
-            }}>Supprimer</button>
-            <button onClick={() => deleteTask(id)} className=" cursor-curseur_custom bg-red-700 p-2 rounded-sm text-white hover:bg-red-900" >Supprimer</button>
+            }} className=" cursor-curseur_custom bg-red-700 p-2 rounded-sm text-white hover:bg-red-900">Supprimer</button>
         </>
     );
 }
