e0335c1a9665e54e61a0951ba2558602fa12f4d0
Alyson Grondin Brochu <2363030@csfoy.ca>
2025-12-10 14:55:47 -0500

    fonction ajouter tache

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,17 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router'
 import TaskList from '../components/TaskList';
 import AjouterTache from '../components/AjouterTache';
+import { useTasks } from '../hooks/useTasks';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const { tasks, addTask } = useTasks();
   return (
     <>
       <h1 className='text-3xl text-center font-mono'>Ma liste de tâches</h1>
-      <AjouterTache/>
-      <TaskList message={'Coucou'}/>
+      <AjouterTache onAdd={addTask} />
+      <TaskList tasks={tasks} message={'Coucou'}/>
     </>
   );
 }
